b659752c08c7e88069e683e88b9cf2a0f7311284
leong <leon.gieringer@gmx.de>
2022-07-08 13:41:58 +0200

    implemented json loader

--- warmstarting/utils/serialization.py
+++ warmstarting/utils/serialization.py
@@ -8,10 +8,12 @@ from ConfigSpace import Configuration
 def serialize_results(score: Dict[str, Union[list, object]],
                       configs: Tuple[Configuration],
                       critical_objects: List[str] = None,
-                      base_path: str = "./results/"):
+                      base_path: str = "./results/",
+                      file_name: str = None):
     """
     serializes the results as well as the corresponding configuration for plotting
     if the configuration contains objects (e.g. torch optimizer) set the parameter respectively
+
     Parameters
     ----------
     score
@@ -22,6 +24,8 @@ def serialize_results(score: Dict[str, Union[list, object]],
         list of keys of all objects in the configuration space
     base_path
         path for saving the file
+    file_name
+        name for saving the file or None
     """
     if critical_objects is None:
         critical_objects = ["optimizer"]
@@ -29,7 +33,8 @@ def serialize_results(score: Dict[str, Union[list, object]],
     if not os.path.exists(base_path):
         os.makedirs(base_path)
 
-    file_name = os.path.join(base_path, "%s" % datetime.now().strftime("%Y%m%d-%H%M%S"))
+    if file_name is None:
+        file_name = os.path.join(base_path, "%s" % datetime.now().strftime("%Y%m%d-%H%M%S"))
 
     # Make configuration object serializable for json
     for c in configs:
@@ -42,3 +47,23 @@ def serialize_results(score: Dict[str, Union[list, object]],
 
     with open(file_name, 'w') as f:
         json.dump(score, f)
+
+
+def load_results(base_path: str = "./results/", file_name: str = "result"):
+    """
+    load serialized results
+
+    Parameters
+    ----------
+    base_path
+        path for loading the file
+    file_name
+        name for loading the file or None
+    """
+    file = os.path.join(base_path, file_name)
+    if not os.path.isfile(file):
+        raise ValueError("File does not exist at given path {}".format(file))
+
+    with open(file, 'r') as f:
+        file_data = json.load(f)
+    return file_data
