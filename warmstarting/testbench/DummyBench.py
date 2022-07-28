af8f0894fb6874b43acfdcb356e2679cf4e63c57
Bronzila <janis_fix@web.de>
2022-07-28 16:14:02 +0200

    Fix SGD setup

--- warmstarting/testbench/DummyBench.py
+++ warmstarting/testbench/DummyBench.py
@@ -62,7 +62,7 @@ class DummyBench(WarmstartingBenchTemplate):
     def init_optim(self, param: nn.Parameter, config: Union[CS.Configuration, Dict],
                    fidelity: Union[CS.Configuration, Dict, None] = None,
                    rng: Union[int, np.random.RandomState, None] = None) -> optim.Optimizer:
-        if type(config["optimizer"]) is optim.SGD:
+        if config["optimizer"] is optim.SGD:
             return config["optimizer"](param, lr=config["lr"], momentum=config["momentum"])
         else:
             return config["optimizer"](param, lr=config["lr"])
